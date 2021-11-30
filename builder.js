6d0b988bbf12c12a9ea456ed2daa188ad706856a
Ahtos <felipele89@gmail.com>
2021-11-30 20:23:38 -0300

    agregando dom y eventos

--- builder.js
+++ builder.js
@@ -1,25 +1,26 @@
-let entrada = parseInt(prompt("Bienvenido, desea construir un Rig? \n 1)si \n 2)no"));
+// let entrada = parseInt(prompt("Bienvenido, desea construir un Rig? \n 1)si \n 2)no"));
 
 
 // procesadores-----------------------------------------
 
 class procesadores {
-    constructor(nombre, marca, socket, chipset, precio, stock) {
+    constructor(nombre, marca, socket, chipset, precio, stock, id) {
         this.nombre = nombre;
         this.marca = marca;
         this.socket = socket;
         this.chipset = parseInt(chipset);
         this.precio = parseInt(precio);
         this.stock = parseInt(stock);
+        this.id = id;
         this.info = function() {alert("Hay " + stock + " unidades en stock del " + nombre + " a $" + precio + " cada uno.")}
     }    
 }
 
-const proce1 = new procesadores("AMD ryzen 5 5600x", "AMD", "AM4", 500, 4000, 10);
-const proce2 = new procesadores("Intel core i3 9300", "INTEL", "1151", 300, 5999, 8);
-const proce3 = new procesadores("AMD ryzen 5 3600", "AMD", "AM4", 300, 4500, 7);
-const proce4 = new procesadores("Intel core i5 10600k", "INTEL", "1200", 300, 9999, 5);
-const proce5 = new procesadores("Intel Celeron G4900", "INTEL", "1151", 300, 12000, 3);
+const proce1 = new procesadores("AMD ryzen 5 5600x", "AMD", "AM4", 500, 4000, 10, "p1");
+const proce2 = new procesadores("Intel core i3 9300", "INTEL", 1151, 300, 5999, 8, "p2");
+const proce3 = new procesadores("AMD ryzen 5 3600", "AMD", "AM4", 300, 4500, 7, "p3");
+const proce4 = new procesadores("Intel core i5 10600k", "INTEL", 1200, 400, 9999, 5, "p4");
+const proce5 = new procesadores("Intel Celeron G4900", "INTEL", 1151, 300, 12000, 3, "p5");
 
 const CPUs = [proce1, proce2, proce3, proce4, proce5];
 
@@ -40,7 +41,7 @@ for(i = 0; i < CPUs.length; i ++) {
 
 
 class motherboards {
-    constructor(nombre, socket, chipset, RAMslots, GPUslots, precio, stock) {
+    constructor(nombre, socket, chipset, RAMslots, GPUslots, precio, stock, id) {
         this.nombre = nombre;
         this.socket = socket;
         this.chipset = chipset;
@@ -48,113 +49,75 @@ class motherboards {
         this.GPUslots = GPUslots;
         this.precio = precio;
         this.stock = stock;
+        this.id = id;
     }       
 }
 
-const mother1 = new motherboards("Gigabyte H410m s1200", 1200, 400, 2, 4, 9999, 4);
-const mother2 = new motherboards("Asus Prime z590-A s1200", 1200, 500, 4, 5, 32500, 3);
-const mother3 = new motherboards("Asus Prime H310m-R", 1151, 300, 2, 2, 8300, 4);
-const mother4 = new motherboards("Asrock A320m-hdv", "AM4", 300, 2, 2, 9000, 2);
-const mother5 = new motherboards("Asrock B450-hdv", "AM4", 999, 2, 2, 9000, 2);   //chipset 999 es que es compatible con chipset 300 400 y 500
-const mother6 = new motherboards("Asus X570-plus", "AM4", 999, 4, 4, 19000, 3);   //chipset 888 es compatible con 400 y 500
+const mother1 = new motherboards("Gigabyte H410m s1200", 1200, 400, 2, 4, 9999, 4, "m1");
+const mother2 = new motherboards("Asus Prime z590-A s1200", 1200, 500, 4, 5, 32500, 3, "m2");
+const mother3 = new motherboards("Asus Prime H310m-R", 1151, 300, 2, 2, 8300, 4, "m3");
+const mother4 = new motherboards("Asrock A320m-hdv", "AM4", 300, 2, 2, 9000, 2, "m4");
+const mother5 = new motherboards("Asrock B450-hdv", "AM4", 999, 2, 2, 9000, 2, "m5");   //chipset 999 es que es compatible con chipset 300 400 y 500
+const mother6 = new motherboards("Asus X570-plus", "AM4", 999, 4, 4, 19000, 3, "m6");   //chipset 888 es compatible con 400 y 500
 
 const mothers = [mother1, mother2, mother3, mother4, mother5, mother6];
 
 
 //ejecutable ---------------------------------------------
 
-while(entrada > 0) {
-    switch(entrada){
-        case 1:
-            let entrada2 = parseInt(prompt("Seleccione la marca \n 1)AMD \n 2)INTEL"));
-            switch(entrada2) {
-                case 1:
-                    let seleccionadosAMD = AMDs.map(nombres => nombres.nombre);
-                    let YAseleccionadosAMD = [];
-                    for ( i = 1; i <= seleccionadosAMD.length; i++) {
-                        
-                        YAseleccionadosAMD.push(i.toString() + ")" + seleccionadosAMD[i-1]);
-                    }
-                    let entrada3 = parseInt(prompt("Excelente, seleccione el numero del que desee para continuar: \n"+ YAseleccionadosAMD.join("\n")));
-                    
-
-                    let seleccionadosMother = [];
-                    for(i = 0; i < AMDs.length; i++) {
-                        if (seleccionadosAMD[entrada3 - 1] === AMDs[i].nombre) {
-                            for (j = 0; j < mothers.length; j++) {
-                                if (AMDs[i].chipset === mothers[j].chipset || AMDs[i].chipset === 999) {
-                                    seleccionadosMother.push(mothers[j].nombre);
-                                }
-                            }
-                            
-                        }
-                    }
-                    
-                    alert("las mother disponibles para ese procesador son: \n" + seleccionadosMother.join("\n"));
-                    
-                    break;
-                case 2:
-                    let seleccionadosINTEL = INTELs.map(nombres => nombres.nombre);
-                    let YAseleccionadosINTEL = [];
-                    for ( i = 1; i <= seleccionadosINTEL.length; i++) {
-                        
-                        YAseleccionadosINTEL.push(i.toString() + ")" + seleccionadosINTEL[i-1]);
-                    }
-                    let entrada4 = parseInt(prompt("Excelente, seleccione el numero del que desee para continuar: \n"+ YAseleccionadosINTEL.join("\n")));
-
-                    let seleccionadosMotheri = [];
-                    for(i = 0; i < INTELs.length; i++) {
-                        if (seleccionadosINTEL[entrada4 - 1] === INTELs[i].nombre) {
-                            for (j = 0; j < mothers.length; j++) {
-                                if (INTELs[i].chipset === mothers[j].chipset || INTELs[i].chipset === 999) {
-                                    seleccionadosMotheri.push(mothers[j].nombre);
-                                }
-                            }
-                            
-                        }
-                    }
-
-                    alert("las mother disponibles para ese procesador son: \n" + seleccionadosMotheri.join("\n"));
-
-                    break;
-                default:
-                    alert("valor ingresado no valido");
-                    break;
-            }
-            break;
-        case 2:
-            alert("hasta la proxima");
-            break;
-        default:
-            alert("valor ingresado no valido, por favor intente devuelta");
-            break;
-    }
-    if (entrada === 2) {
-        break;
-    }
-    entrada = parseInt(prompt("Bienvenido, desea construir un Rig? \n 1)si \n 2)no"));
-}
-
 // stock:
-
+let contenedorCPU = "";
+let seleccionado = "";
+let mothersSeleccionadas = [];
 for (const cpu of CPUs) {
 
-    let contenedorCPU = document.createElement("div");
+    contenedorCPU = document.createElement("div");
+
+    contenedorCPU.setAttribute("id", cpu.id);
+    contenedorCPU.setAttribute("onclick", "hacerAlgo(this.id)");
+    
 
     contenedorCPU.innerHTML = `<h5>${cpu.nombre}</h5>
                             <p> Precio: $${cpu.precio}</p>
-                            <b> Stock inmediato: ${cpu.stock}</b>`;
+                            <b> Stock: ${cpu.stock}</b>`;
 
     document.body.appendChild(contenedorCPU);
+    contenedorCPU.classList.add("procesadoresEnStock");
+    
 }
 
-for (const mot of mothers) {
+function hacerAlgo(x){
+    console.log(x);
+
+    for (let i=0; i < CPUs.length; i++) {
+        if (CPUs[i].id === x) {
+            seleccionado = i;
+        }
+    }
+
+    const borrando = document.getElementsByClassName("procesadoresEnStock");
+    while (borrando.length > 0) borrando [0].remove();
+
+    for (let i = 0; i < mothers.length; i++) {
+        if ((mothers[i].chipset === CPUs[seleccionado].chipset || mothers[i].chipset === 999) && mothers[i].socket === CPUs[seleccionado].socket) {
+            mothersSeleccionadas.push(mothers[i]);
+        }
+    }
+
+    for (const placasMadre of mothersSeleccionadas) {
+
+        contenedorMothers = document.createElement("div");
     
-    let contenedorMOT = document.createElement("div");
-
-    contenedorMOT.innerHTML = `<h5>${mot.nombre}</h5>
-                            <p> Precio: $${mot.precio}</p>
-                            <b> Stock: ${mot.stock}</b>`;
-
-    document.body.appendChild(contenedorMOT);
+        contenedorMothers.setAttribute("id", placasMadre.id);
+        // contenedorMothers.setAttribute("onclick", alert("mostrar rams compatibles"));
+        
+    
+        contenedorMothers.innerHTML = `<h5>${placasMadre.nombre}</h5>
+                                <p> Precio: $${placasMadre.precio}</p>
+                                <b> Stock: ${placasMadre.stock}</b>`;
+    
+        document.body.appendChild(contenedorMothers);
+        contenedorMothers.classList.add("procesadoresEnStock");
+        
+    }
 }
